5f2238144869f54f3c09dd0c6c4b14de765ae863
dawnlessdays <dawnlessdays.id@gmail.com>
2025-07-26 19:38:33 +0800

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,1 +1,11 @@
+// src/components/Sidebar.jsx
+import React from 'react'
+
+const Sidebar = () => (
+  <div className="w-64 bg-gray-900 text-white p-4">
+    <p className="font-bold">Dawnlessday</p>
+  </div>
+)
+
+export default Sidebar
 

34d682168389207c0a8d2ce8d5d56a15bd7421dc
dawnlessdays <dawnlessdays.id@gmail.com>
2025-07-26 19:38:54 +0800

    Update topbar.jsx

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,1 +1,12 @@
 
+// src/components/Topbar.jsx
+import React from 'react'
+
+const Topbar = () => (
+  <div className="bg-white shadow p-4 flex justify-between items-center">
+    <p className="font-semibold">Home</p>
+    <div>User</div>
+  </div>
+)
+
+export default Topbar
